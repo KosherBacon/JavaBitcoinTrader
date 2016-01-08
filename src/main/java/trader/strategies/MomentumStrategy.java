1043f703f292e3d303131713e1fb6605960e0136
Joshua Kahn <j.harris.kahn@gmail.com>
2016-01-08 02:32:31 -0800

    Adjusted imports to accomodate package change.

--- src/main/java/trader/strategies/MomentumStrategy.java
+++ src/main/java/trader/strategies/MomentumStrategy.java
@@ -1,10 +1,17 @@
 package trader.strategies;
 
+import eu.verdelhan.ta4j.Decimal;
 import eu.verdelhan.ta4j.Rule;
 import eu.verdelhan.ta4j.Strategy;
 import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.oscillators.AroonDownIndicator;
+import eu.verdelhan.ta4j.indicators.oscillators.AroonUpIndicator;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.RSIIndicator;
+import eu.verdelhan.ta4j.trading.rules.*;
 import org.jetbrains.annotations.Contract;
 import org.jooq.lambda.tuple.Tuple2;
+import trader.indicators.CMOIndicator;
 
 /**
  * Created by Joshua Kahn on 1/7/2016.
@@ -35,7 +42,26 @@ public class MomentumStrategy extends Strategy {
         if (series == null) {
             throw new IllegalArgumentException("Series cannot be null");
         }
-        return null;
+
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+
+        // Chande Moving Oscillator
+        CMOIndicator cmoIndicator = new CMOIndicator(series, 14);
+
+        // Aroon indicators
+        AroonUpIndicator aroonUpIndicator = new AroonUpIndicator(series, 25);
+        AroonDownIndicator aroonDownIndicator = new AroonDownIndicator(series, 25);
+
+        RSIIndicator rsi = new RSIIndicator(closePrice, 14);
+
+        Rule entryRule = new OverIndicatorRule(aroonUpIndicator, aroonDownIndicator).and(new CrossedUpIndicatorRule(rsi, Decimal.valueOf(30)));
+
+        // Protect assets on loss
+        Rule stopLoss = new StopLossRule(closePrice, Decimal.valueOf(15));
+
+        Rule exitRule = new AndRule(new OverIndicatorRule(aroonDownIndicator, aroonUpIndicator), new CrossedDownIndicatorRule(rsi, Decimal.valueOf(70))).or(stopLoss);
+
+        return new Tuple2<>(entryRule, exitRule);
     }
 
 }
