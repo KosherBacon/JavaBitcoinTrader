2955191cf5e9dcb2a40a95bfb9015a8f09d3a471
Joshua Kahn <j.harris.kahn@gmail.com>
2016-01-08 02:54:17 -0800

    Temporary graphing of CMO.

---
+++ src/main/java/trader/IndicatorsToChart.java
@@ -0,0 +1,129 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2015 - 2016 Joshua Kahn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package trader;
+
+import eu.verdelhan.ta4j.Decimal;
+import eu.verdelhan.ta4j.Indicator;
+import eu.verdelhan.ta4j.Tick;
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+import trader.indicators.CMOIndicator;
+import trader.exchanges.backtest.BacktestLoader;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * This class builds a graphical chart showing values from indicators.
+ */
+public class IndicatorsToChart {
+
+    /**
+     * Builds a JFreeChart time series from a Ta4j time series and an indicator.
+     * @param tickSeries the ta4j time series
+     * @param indicator the indicator
+     * @param name the name of the chart time series
+     * @return the JFreeChart time series
+     */
+    private static org.jfree.data.time.TimeSeries buildChartTimeSeries(TimeSeries tickSeries, Indicator<Decimal> indicator, String name) {
+        org.jfree.data.time.TimeSeries chartTimeSeries = new org.jfree.data.time.TimeSeries(name);
+        for (int i = 0; i < tickSeries.getTickCount(); i++) {
+            Tick tick = tickSeries.getTick(i);
+            chartTimeSeries.addOrUpdate(new Day(tick.getEndTime().toDate()), indicator.getValue(i).toDouble());
+        }
+        return chartTimeSeries;
+    }
+
+    /**
+     * Displays a chart in a frame.
+     * @param chart the chart to be displayed
+     */
+    private static void displayChart(JFreeChart chart) {
+        // Chart panel
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setFillZoomRectangle(true);
+        panel.setMouseWheelEnabled(true);
+        panel.setPreferredSize(new java.awt.Dimension(500, 270));
+        // Application frame
+        ApplicationFrame frame = new ApplicationFrame("BTC/USD with Chande Moving Oscillator");
+        frame.setContentPane(panel);
+        frame.pack();
+        RefineryUtilities.centerFrameOnScreen(frame);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        /**
+         * Getting time series
+         */
+        TimeSeries series = BacktestLoader.loadSeries("bitfinexUSD.csv");
+
+        /**
+         * Creating indicators
+         */
+        // Close price
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+        // Chande Moving Oscillator
+        CMOIndicator cmoIndicator = new CMOIndicator(series, 14);
+
+
+        /**
+         * Building chart dataset
+         */
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(buildChartTimeSeries(series, closePrice, "BTC/USD - Bitfinex"));
+        dataset.addSeries(buildChartTimeSeries(series, cmoIndicator, "Chande Moving Oscillator"));
+
+        /**
+         * Creating the chart
+         */
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "BTC/USD Bitfinex Close Prices", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                true, // create legend?
+                true, // generate tooltips?
+                false // generate URLs?
+        );
+        XYPlot plot = (XYPlot) chart.getPlot();
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));
+
+        /**
+         * Displaying the chart
+         */
+        displayChart(chart);
+    }
+
+}
