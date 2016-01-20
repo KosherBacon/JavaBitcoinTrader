715cc2a7132eccff6e893a4b7a39ee3616a80c54
Joshua Kahn <j.harris.kahn@gmail.com>
2016-01-20 14:46:48 -0500

    Added a lot of comments. Also vastly improved strategy profitability.

--- src/main/java/trader/strategies/BasicStrategy.java
+++ src/main/java/trader/strategies/BasicStrategy.java
@@ -23,14 +23,19 @@
 
 package trader.strategies;
 
-import eu.verdelhan.ta4j.*;
-import eu.verdelhan.ta4j.indicators.candles.*;
+import eu.verdelhan.ta4j.Decimal;
+import eu.verdelhan.ta4j.Rule;
+import eu.verdelhan.ta4j.Strategy;
+import eu.verdelhan.ta4j.TimeSeries;
 import eu.verdelhan.ta4j.indicators.oscillators.StochasticOscillatorKIndicator;
 import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
-import eu.verdelhan.ta4j.indicators.trackers.*;
+import eu.verdelhan.ta4j.indicators.trackers.EMAIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.MACDIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.SMAIndicator;
 import eu.verdelhan.ta4j.trading.rules.*;
 import org.jetbrains.annotations.Contract;
 import org.jooq.lambda.tuple.Tuple2;
+import trader.indicators.CMOIndicator;
 
 /**
  * Created by jkahn on 12/22/15.
@@ -42,17 +47,56 @@ public class BasicStrategy extends Strategy {
     /**
      * The number of ticks needed for the strategy to be fully functional.
      */
-    public static final int TICKS_NEEDED = 225;
+    public static final int TICKS_NEEDED = 205;
 
     /**
-     * The number of periods to use for both the crowes and soldiers
-     * candlestick analysis.
+     * The number of periods to use for the Chande Momentum Oscillator.
      */
-    private static final int CROWES_AND_SOLDIERS_PERIODS = 4;
+    private static final int CMO_PERIODS = 2;
 
-    private static final int RSI_PERIODS = 2;
-    private static final int RSI_UPPER = 95;
-    private static final int RSI_LOWER = 5;
+    /**
+     * The upper bound to use for the Chande Momentum Oscillator
+     */
+    private static final int CMO_UPPER = 70;
+
+    /**
+     * The lower bound to use for the Chande Momentum Oscillator
+     */
+    private static final int CMO_LOWER = -70;
+
+    /**
+     * The number of periods to use for the short running part of the MACD.
+     */
+    private static final int MACD_SHORT_PERIODS = 9;
+
+    /**
+     * The number of periods to use for the long running part of the MACD.
+     */
+    private static final int MACD_LONG_PERIODS = 26;
+
+    /**
+     * The number of periods to use for the short running SMA.
+     */
+    private static final int SMA_SHORT_PERIODS = 5;
+
+    /**
+     * The number of periods to use for the long running SMA.
+     */
+    private static final int SMA_LONG_PERIODS = 200;
+
+    /**
+     * The threshold (in percent) for the maximum loss allowed on a given
+     * trade. This value protects against severe losses, lower values should be
+     * used for more conservative trading.
+     */
+    private static final int STOP_LOSS_THRESHOLD = 10;
+
+    /**
+     * The threshold (in percent) for the maximum gain allowed on a given
+     * trade. This value protects the gains from a trade, lower values should
+     * be used for more conservative trading.
+     */
+    private static final int STOP_GAIN_THRESHOLD = 10;
 
     public BasicStrategy(TimeSeries series) {
         this(buildStrategy(series));
@@ -78,64 +122,61 @@ public class BasicStrategy extends Strategy {
         }
 
         ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
-        SMAIndicator shortSma = new SMAIndicator(closePrice, 5);
-        SMAIndicator longSma = new SMAIndicator(closePrice, 200);
 
-        // We use a 2-period RSI indicator to identify buying
-        // or selling opportunities within the bigger trend.
-        RSIIndicator rsi = new RSIIndicator(closePrice, RSI_PERIODS);
+        SMAIndicator shortSma = new SMAIndicator(closePrice, SMA_SHORT_PERIODS);
+        SMAIndicator longSma = new SMAIndicator(closePrice, SMA_LONG_PERIODS);
 
-        // The bias is bearish when the shorter-moving average moves below
-        // the longer moving average.
+        CMOIndicator cmo = new CMOIndicator(series, CMO_PERIODS);
+
+        // The bias is bearish when the shorter-moving averagemoves below the
+        // longer moving average.
         EMAIndicator shortEma = new EMAIndicator(closePrice, 9);
         EMAIndicator longEma = new EMAIndicator(closePrice, 26);
 
         StochasticOscillatorKIndicator stochasticOscillK = new
                 StochasticOscillatorKIndicator(series, 14);
 
-        MACDIndicator macd = new MACDIndicator(closePrice, 9, 26);
+        MACDIndicator macd = new MACDIndicator(closePrice, MACD_SHORT_PERIODS,
+                MACD_LONG_PERIODS);
         EMAIndicator emaMacd = new EMAIndicator(macd, 18);
 
-        Rule macdEntry = new OverIndicatorRule(shortEma, longEma) // Trend
-                .and(new CrossedDownIndicatorRule(stochasticOscillK, Decimal
-                        .valueOf(20))) // Signal 1
-                .and(new OverIndicatorRule(macd, emaMacd)); // Signal 2
+        Rule momentumEntry = new OverIndicatorRule(shortSma, longSma) // Trend
+                // Signal 1
+                .and(new CrossedDownIndicatorRule(cmo,
+                        Decimal.valueOf(CMO_LOWER)))
+                // Signal 2
+                .and(new OverIndicatorRule(shortEma, closePrice));
 
-        Rule crowesRule = new BooleanIndicatorRule(new
-                ThreeBlackCrowsIndicator(series, CROWES_AND_SOLDIERS_PERIODS,
-                Decimal.valueOf(7)));
+        // Entry rule
+        Rule entryRule = new OverIndicatorRule(shortEma, longEma) // Trend
+                // Signal 1
+                .and(new CrossedDownIndicatorRule(stochasticOscillK,
+                        Decimal.valueOf(20)))
+                // Signal 2
+                .and(new OverIndicatorRule(macd, emaMacd))
+                .or(momentumEntry);
 
-        Rule candleEntry = new OrRule(crowesRule, new BooleanIndicatorRule
-                (new BullishHaramiIndicator(series)));
+        Rule momentumExit = new UnderIndicatorRule(shortSma, longSma) // Trend
+                // Signal 1
+                .and(new CrossedUpIndicatorRule(cmo,
+                        Decimal.valueOf(CMO_UPPER)))
+                // Signal 2
+                .and(new UnderIndicatorRule(shortSma, closePrice));
 
-        Rule rsiEntry = new OverIndicatorRule(shortEma, longEma) // Trend
-                .and(new CrossedDownIndicatorRule(rsi, Decimal.valueOf
-                        (RSI_LOWER))) // Signal 1
-                .and(new OverIndicatorRule(shortEma, closePrice)); // Signal 2
-
-        Rule entryRule = new OrRule(macdEntry, rsiEntry)
-                .or(candleEntry);
-
-        Rule macdExit = new UnderIndicatorRule(shortEma, longEma) // Trend
-                .and(new CrossedUpIndicatorRule(stochasticOscillK, Decimal
-                        .valueOf(80))) // Signal 1
-                .and(new UnderIndicatorRule(macd, emaMacd)); // Signal 2
-
-        Rule soldiersRule = new BooleanIndicatorRule(new
-                ThreeWhiteSoldiersIndicator(series,
-                CROWES_AND_SOLDIERS_PERIODS, Decimal.valueOf(7)));
-
-        Rule rsiExit = new UnderIndicatorRule(shortSma, longSma) // Trend
-                .and(new CrossedUpIndicatorRule(rsi, Decimal.valueOf
-                        (RSI_UPPER))) // Signal 1
-                .and(new UnderIndicatorRule(shortSma, closePrice)); // Signal 2
-
-        Rule exitRule = new OrRule(macdExit, rsiExit)
-                .or(soldiersRule)
-                .or(new StopLossRule(closePrice, Decimal.valueOf(15))) //
+        // Exit rule
+        Rule exitRule = new UnderIndicatorRule(shortEma, longEma) // Trend
+                // Signal 1
+                .and(new CrossedUpIndicatorRule(stochasticOscillK,
+                        Decimal.valueOf(80)))
+                // Signal 2
+                .and(new UnderIndicatorRule(macd, emaMacd))
+                .or(momentumExit)
                 // Protect against severe losses
-                .or(new StopGainRule(closePrice, Decimal.valueOf(10))); //
-        // Take profits and run
+                .or(new StopLossRule(closePrice, Decimal.valueOf
+                        (STOP_LOSS_THRESHOLD)))
+                // Take profits and run
+                .or(new StopGainRule(closePrice, Decimal.valueOf
+                        (STOP_GAIN_THRESHOLD)));
 
         return new Tuple2<>(entryRule, exitRule);
     }
